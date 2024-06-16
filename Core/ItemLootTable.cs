c35543ca6b08ba5715f3150658cc855976643e38
SangWon Park <shshck5@gmail.com>
2024-06-17 00:43:06 +0900

    Add files via upload

---
+++ Core/ItemLootTable.cs
@@ -0,0 +1,164 @@
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class ItemLootTable : MonoBehaviour
+{
+    // [ID] 0. All Random / 1. Common / 2. Uncommon / 3. Rare / 4. Boss
+    [SerializeField] private List<ItemList> itemLists = new();
+
+    /// <summary>
+    /// 아이템 등급별 또는 모든 중류 랜덤 생성
+    /// </summary>
+    public ItemBlueprint GetItemAdd(int rareTypeID)
+    {
+        return rareTypeID switch
+        {
+            1 => this.itemLists[0].GetRandomItem(),
+            2 => this.itemLists[1].GetRandomItem(),
+            3 => this.itemLists[2].GetRandomItem(),
+            4 => this.itemLists[3].GetRandomItem(),
+            _ => GetRandomItemToAllList(),
+        };
+    }
+
+    /// <summary>
+    /// 아이템 등급별로 랜덤 중복 생성
+    /// </summary>
+    public ItemBlueprint GetItemAdd_Duplicate(int rareTypeID)
+    {
+        return rareTypeID switch
+        {
+            1 => this.itemLists[0].GetRandomItem_Duplicate(),
+            2 => this.itemLists[1].GetRandomItem_Duplicate(),
+            3 => this.itemLists[2].GetRandomItem_Duplicate(),
+            4 => this.itemLists[3].GetRandomItem_Duplicate(),
+            _ => GetRandomItemToAllList_Duplicate(),
+        };
+    }
+
+    /// <summary>
+    /// 모든 아이템 리스트 중에서 갯수 만큼 중복없이 생성
+    /// </summary>
+    public ItemBlueprint[] GetItemTable(int amount)
+    {
+        var cloneLists = this.itemLists.ConvertAll(list => list.Clone()).ToList();
+        var totalWeight = cloneLists.Sum(cloneList => cloneList.weight);
+
+        var randomItemBlueprints = new ItemBlueprint[amount];
+
+        for (int i = 0; i < amount; i++)
+        {
+            randomItemBlueprints[i] = GetWeightedItem(cloneLists, totalWeight);
+        }
+
+        return randomItemBlueprints;
+    }
+
+    /// <summary>
+    /// 아이템 리스트 중에서 가중치에 따른 랜덤 아이템 생성 후 해당 아이템 삭제
+    /// </summary>
+    public ItemBlueprint GetRandomItemToAllList()
+    {
+        var totalWeight = this.itemLists.Sum(itemList => itemList.weight);
+
+        return GetWeightedItem(this.itemLists, totalWeight);
+    }
+
+    /// <summary>
+    /// 아이템 리스트 중에서 가중치에 따른 랜덤 아이템 생성 (중복 가능)
+    /// </summary>
+    public ItemBlueprint GetRandomItemToAllList_Duplicate()
+    {
+        var totalWeight = this.itemLists.Sum(itemList => itemList.weight);
+
+        return GetWeightedItem_Duplicate(this.itemLists, totalWeight);
+    }
+
+    /// <summary>
+    /// 아이템 리스트에서 랜덤으로 아이템 생성 후 해당 아이템 삭제
+    /// </summary>
+    /// <param name="itemLists"></param>
+    /// <param name="totalWeight"></param>
+    public ItemBlueprint GetWeightedItem(List<ItemList> itemLists, float totalWeight)
+    {
+        var randomWeight = Random.Range(0f, totalWeight);
+
+        foreach (var itemList in itemLists)
+        {
+            if (itemList.weight >= randomWeight)
+            {
+                return itemList.GetRandomItem();
+            }
+
+            randomWeight -= itemList.weight;
+        }
+
+        throw new System.Exception("Weighted Item Generate Failed");
+    }
+
+    /// <summary>
+    /// 아이템 리스트에서 랜덤으로 아이템 생성 (중복 가능)
+    /// </summary>
+    /// <param name="itemLists"></param>
+    /// <param name="totalWeight"></param>
+    public ItemBlueprint GetWeightedItem_Duplicate(List<ItemList> itemLists, float totalWeight)
+    {
+        var randomWeight = Random.Range(0f, totalWeight);
+
+        foreach (var itemList in itemLists)
+        {
+            if (itemList.weight >= randomWeight)
+            {
+                return itemList.GetRandomItem_Duplicate();
+            }
+
+            randomWeight -= itemList.weight;
+        }
+
+        throw new System.Exception("Weighted Item_Duplicate Generate Failed");
+    }
+}
+
+#region Item List Class
+[System.Serializable]
+public class ItemList
+{
+    public string listName;
+    public List<ItemBlueprint> itemList;
+    public float weight;
+
+    public ItemList Clone()
+    {
+        return new ItemList 
+        { 
+            listName = this.listName, 
+            itemList = this.itemList.ToList(), // 값 복사
+            weight = this.weight 
+        };
+    }
+
+    /// <summary>
+    /// 리스트에서 랜덤으로 데이터 가져온 후 해당 데이터 삭제
+    /// </summary>
+    /// <returns></returns>
+    public ItemBlueprint GetRandomItem()
+    {
+        var random = Random.Range(0, this.itemList.Count);
+        var randomItem = this.itemList[random];
+        this.itemList.Remove(randomItem);
+        return randomItem;
+    }
+
+    /// <summary>
+    /// 리스트에서 랜덤으로 아이템 데이터 가져오기 (중첩 가능)
+    /// </summary>
+    /// <returns></returns>
+    public ItemBlueprint GetRandomItem_Duplicate()
+    {
+        var random = Random.Range(0, this.itemList.Count);
+        var randomItem = this.itemList[random];
+        return randomItem;
+    }
+}
+#endregion
---
+++ Core/SceneLoader.cs
@@ -0,0 +1,138 @@
+using PSW.Core.Extensions;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+/// <summary>
+/// Scene 로딩하면서 화면에 특정 애니메이션 작동
+/// </summary>
+public class SceneLoader : BehaviourSingleton<SceneLoader>
+{
+    // 로딩 하는 동안 다른 동작 방지용
+    public static bool IsLoaded { get; set; }
+
+    [SerializeField] private RectTransform sceneSize;
+    [SerializeField] private GridLayoutGroup gridLayout;
+
+    [Header("Blind")]
+    [SerializeField] private Transform[] blinds;
+    [SerializeField] private float fadeDuration = 0.5f;
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        IsLoaded = true;
+
+        SetBlindSize();
+    }
+
+    private void SetBlindSize()
+    {
+        var sizeX = this.sceneSize.sizeDelta.x / 3.0f;
+        var sizeY = this.sceneSize.sizeDelta.y / 7.0f;
+
+        this.gridLayout.cellSize = new Vector2(sizeX, sizeY);
+    }
+
+    /// <summary>
+    /// 씬 마무리 => 플레이어 유무에 따른 씬 체크
+    /// </summary>
+    /// <param name="sceneName"></param>
+    public void PlayerCheckSceneLoad(string sceneName)
+    {
+        // 플레이어 정보 있을 시 => 해당 씬 해제
+        if (Player.Instance != null)
+        {
+            UnLoadAdditiveScene(sceneName);
+
+            // 스테이지로 복귀시 BGM 변경
+            AudioBGM.Instance.BGMChange(AudioBGM.Instance.stage);
+        }
+        // 플레이어 정보 없을 시 => 해당 씬 재진입 (테스트 용)
+        else
+        {
+            LoadScene(sceneName);
+        }
+    }
+
+    /// <summary>
+    /// 씬에서 씬으로 자유 이동
+    /// </summary>
+    /// <param name="sceneName"></param>
+    public void LoadScene(string sceneName)
+    {
+        StopAllCoroutines();
+        StartCoroutine(AnimationFadeScene(LoadSceneCoroutine(sceneName)));
+    }
+
+    /// <summary>
+    /// 코루틴 동안 Scene 화면에 페이딩 부드럽게
+    /// </summary>
+    /// <param name="sceneName"></param>
+    /// <returns></returns>
+    private IEnumerator LoadSceneCoroutine(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName, LoadSceneMode.Single);     
+
+        yield return YieldCache.WaitForSeconds(0.5f);
+    }
+
+    /// <summary>
+    /// 스테이지 씬에서 씬 추가 (Starting, Battle, Shop, Mystery, RestSite, Treasure)
+    /// </summary>
+    /// <param name="sceneName"></param>
+    public void LoadAdditiveScene(string sceneName)
+    {
+        StopAllCoroutines();
+        StartCoroutine(AnimationFadeScene(LoadAdditiveSceneCoroutine(sceneName)));
+    }
+
+    private IEnumerator LoadAdditiveSceneCoroutine(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName, LoadSceneMode.Additive);
+        
+        yield return YieldCache.WaitForSeconds(0.5f);
+
+        StageSystem.Instance.StageActive(false);
+    }
+
+    /// <summary>
+    /// 스테이지 씬에서 추가된 씬 다시 해제
+    /// </summary>
+    /// <param name="sceneName"></param>
+    public void UnLoadAdditiveScene(string sceneName)
+    {
+        StopAllCoroutines();
+        StartCoroutine(AnimationFadeScene(UnLoadAdditiveSceneCoroutine(sceneName)));
+    }
+
+    private IEnumerator UnLoadAdditiveSceneCoroutine(string sceneName)
+    {
+        SceneManager.UnloadSceneAsync(sceneName);
+        
+        yield return YieldCache.WaitForSeconds(0.5f);
+
+        StageSystem.Instance.StageActive(true);        
+    }
+
+    /// <summary>
+    /// 씬 전환시 작동하는 페이드 애니메이션
+    /// </summary>
+    /// <param name="coroutine"></param>
+    /// <returns></returns>
+    private IEnumerator AnimationFadeScene(IEnumerator coroutine)
+    {
+        IsLoaded = false;
+        
+        yield return StartCoroutine(this.blinds.FadeInCoroutine(this.fadeDuration));
+
+        SettingsSystem.Instance.Init();
+        yield return StartCoroutine(coroutine);
+
+        yield return StartCoroutine(this.blinds.FadeOutCoroutine(this.fadeDuration));
+        
+        IsLoaded = true;
+    }
+}
